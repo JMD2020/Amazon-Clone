bab5257a7bb8830258eef89ddbbc3ea0b5f6040a
JMD2020 <98502524+JMD2020@users.noreply.github.com>
2022-05-29 16:10:49 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,11 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsC3f5-ITl79ur-QS-l3UpnTO4QpWPBb4",
+   
     authDomain: "clone-26fa8.firebaseapp.com",
     databaseURL: "https://amazon-clone-26fa8.firebaseio.com",
     projectId: "clone-26fa8",
     storageBucket: "clone-26fa8.appspot.com",
-    messagingSenderId: "610452975532",
-    appId: "1:610452975532:web:70637bb5ebbcfde2889409",
-    measurementId: "G-0FNWWG67JV",
   };
   
     
